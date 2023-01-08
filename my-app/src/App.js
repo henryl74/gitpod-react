c40c05f4e944b9a876882c4d40690970e11070e2
Henry Leon <henryatusabes@gmail.com>
2023-01-08 20:53:13 +0000

    Binding Events for State Updates

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,16 +3,18 @@ import "./App.css";
 // import FunctionalGreeting from './components/FunctionalGreeting';
 // import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
 // import StatefulGreetingWithPrevState from "./components/StatefulGreetingWIthPrevState";
-import EventsClass from "./components/EventsClass";
-import EventsFunctional from "./components/EventsFunctional";
+// import EventsClass from "./components/EventsClass";
+// import EventsFunctional from "./components/EventsFunctional";
+import EventBinding from "./components/EventBinding";
 
 function App() {
   return ( 
     <div className="App">
     {/* <FunctionalGreetingWithProps greeting="nice to meet you!" name="Mike!" age="32"/> */}
     {/* <StatefulGreetingWithPrevState /> */}
-    <EventsFunctional />
-    <EventsClass />
+    {/* <EventsFunctional /> */}
+    {/* <EventsClass /> */}
+    <EventBinding />
     </div >
   );
 }
