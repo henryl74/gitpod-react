dc09b292a027c2ce74b1f4c1cdd01c748c7241e3
Henry Leon <henryatusabes@gmail.com>
2023-01-08 20:03:48 +0000

    Add Handling Events in React

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,13 +2,17 @@
 import "./App.css";
 // import FunctionalGreeting from './components/FunctionalGreeting';
 // import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
-import StatefulGreetingWithPrevState from "./components/StatefulGreetingWIthPrevState";
+// import StatefulGreetingWithPrevState from "./components/StatefulGreetingWIthPrevState";
+import EventsClass from "./components/EventsClass";
+import EventsFunctional from "./components/EventsFunctional";
 
 function App() {
   return ( 
     <div className="App">
     {/* <FunctionalGreetingWithProps greeting="nice to meet you!" name="Mike!" age="32"/> */}
-    <StatefulGreetingWithPrevState />
+    {/* <StatefulGreetingWithPrevState /> */}
+    <EventsFunctional />
+    <EventsClass />
     </div >
   );
 }
