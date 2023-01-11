969ea0a50a7a480b1291ea272a26994a35d62ca8
Henry Leon <henryatusabes@gmail.com>
2023-01-11 21:20:14 +0000

    Add Nesting Components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,8 +6,9 @@ import "./App.css";
 // import EventsClass from "./components/EventsClass";
 // import EventsFunctional from "./components/EventsFunctional";
 // import EventBinding from "./components/EventBinding";
-import ConditionalRenderingClass from "./components/ConditionalRenderingClass";
-import ConditionalRenderingFunctional from "./components/ConditionalRenderingFunctional";
+// import ConditionalRenderingClass from "./components/ConditionalRenderingClass";
+// import ConditionalRenderingFunctional from "./components/ConditionalRenderingFunctional";
+import NestingComponents from "./components/NestingComponents";
 
 function App() {
   return ( 
@@ -17,8 +18,9 @@ function App() {
     {/* <EventsFunctional /> */}
     {/* <EventsClass /> */}
     {/* <EventBinding /> */}
-    <ConditionalRenderingClass />
-    <ConditionalRenderingFunctional connected={true} />
+    {/* <ConditionalRenderingClass /> */}
+    {/* <ConditionalRenderingFunctional connected={true} /> */}
+    <NestingComponents />
     </div >
   );
 }
